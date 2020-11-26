027ad8d97fe47aae11df93e342aa9c6d3ada67a0
sujitIwale <56259235+sujitIwale@users.noreply.github.com>
2020-11-26 23:35:59 +0530

    connecting

--- src/chatApp/Client.java
+++ src/chatApp/Client.java
@@ -5,29 +5,25 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
-class User {
-	 String userName  ="";
-	 
-	 String getuserName() {
-     	return userName;
-     }
-}
 
 public class Client {
 
 	public static void main(String[] args) throws IOException {
 		Scanner scan = new Scanner(System.in);
 		Socket s = new Socket("localhost", 3002);
-        User user = new User();
+  
+       
 		PrintWriter pr = new PrintWriter(s.getOutputStream());
 		System.out.print("please enter a user name :");
-		user.userName = scan.nextLine();
-		 
+		String name = scan.nextLine();
+		
+		
 		System.out.print("enter message: ");
 		String input = scan.nextLine();
-		pr.print(input);
+		String value = name + "~" + input;
+		pr.println(value);
 		pr.flush();
-
+        
 
 	}
 
--- src/chatApp/Server.java
+++ src/chatApp/Server.java
@@ -9,19 +9,23 @@ public class Server {
 	public static void main(String[] args) throws IOException {
 		
 		Scanner scan = new Scanner(System.in);
-		System.out.println("waiting for client");
+		
 		ServerSocket ss = new ServerSocket(3002);
+		System.out.println("waiting for client");
 		Socket s = ss.accept();
-        User user = new User();
-        
-        String userName = user.getuserName();
+    
         
 		System.out.println("Client connected");
 		InputStreamReader in = new InputStreamReader(s.getInputStream());
 		BufferedReader bf = new BufferedReader(in); 
-
+		
 		String str = bf.readLine();
-		System.out.println( userName +": " + str); 
+		String inputValue[] = str.split("~");
+		String userName = inputValue[0];
+		String msg = inputValue[1];
+		System.out.println( userName +": " + msg); 
+		
+		System.out.println(userName);
 	}
 
 }
